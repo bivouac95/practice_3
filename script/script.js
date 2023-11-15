8f11c77cbe0614fe709ebb4f9790394863bdd144
bivouac95 <tivan0408@gmail.com>
2023-11-15 15:08:50 +0900

    implomenting aside menu

--- script/script.js
+++ script/script.js
@@ -1,19 +1,23 @@
-// //включение меню
-// document.getElementById("amongus").addEventListener("click", () => {
-//   document.getElementById("menu").classList.toggle("menu-active");
-// });
+//включение меню
+document.getElementById("amongus").addEventListener("click", () => {
+  document.getElementById("menu").classList.toggle("menu-active");
+});
 
-// //стилизация навигационных кнопок
-// const navItem = document.querySelectorAll(".navigation-link");
+document.getElementById("anti-amongus").addEventListener("click", () => {
+  document.getElementById("menu").classList.toggle("menu-active");
+});
 
-// for (let i = 0; i < navItem.length; i++) {
-//   navItem[i].addEventListener("click", () => {
-//     for (let j = 0; j < navItem.length; j++) {
-//       navItem[j].classList.remove("navigation-link--active");
-//     }
-//     navItem[i].classList.add("navigation-link--active");
-//   });
-// }
+//стилизация навигационных кнопок
+const navItem = document.querySelectorAll(".navigation-link");
+
+for (let i = 0; i < navItem.length; i++) {
+  navItem[i].addEventListener("click", () => {
+    for (let j = 0; j < navItem.length; j++) {
+      navItem[j].classList.remove("navigation-link--active");
+    }
+    navItem[i].classList.add("navigation-link--active");
+  });
+}
 
 //читать далее
 document.getElementById("article__read-more").addEventListener("click", () => {
